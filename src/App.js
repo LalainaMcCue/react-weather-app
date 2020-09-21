78cdd62da371708a60f1f0c10485a7202e66c128
Lalaina McCue <68620211+LalainaMcCue@users.noreply.github.com>
2020-09-21 17:33:39 -0400

    Added search bar event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Search from "./Search";
-import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <Search />
-        <Weather defaultCity="Cleveland"/>
       </div>
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import './Search.css';
+import Weather from "./Weather";
 
 export default function Search() {
+  const [city, setCity] = useState("Cleveland");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(city);
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="Search">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input className="myLocation" type="submit" value="📍" />
         <input
           className="search-bar"
           type="search"
           placeholder="Enter a city..."
+          onChange={handleCityChange}
         />
         <input className="search-button" type="submit" value="Search" />
       </form>
+      <Weather defaultCity = {city} />
     </div>
   );
 }
