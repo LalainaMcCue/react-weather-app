3165a20bedfb5964783b903f531a4c2b3442c55a
Lalaina McCue <68620211+LalainaMcCue@users.noreply.github.com>
2020-09-24 10:44:18 -0400

    Search Functionality

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,38 @@
 import React, {useState} from "react";
 import './Search.css';
 import Weather from "./Weather";
+import axios from "axios";
 
 export default function Search() {
   const [city, setCity] = useState("Cleveland");
+  const [weatherData, setWeatherData] = useState({ready : false});
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert(city);
+    const apiKey = "7f2bcb3fa67c76b6d051afd4ec0b0d33";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(weatherResponse);
+    
   }
 
   function handleCityChange(event) {
     setCity(event.target.value);
   }
-
+  
+  function weatherResponse(response) {
+    console.log(response);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      iconUrl: "",
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+   });
+ }
+ if (weatherData.ready) {
   return (
     <div className="Search">
       <form onSubmit={handleSubmit}>
@@ -26,7 +45,13 @@ export default function Search() {
         />
         <input className="search-button" type="submit" value="Search" />
       </form>
-      <Weather defaultCity = {city} />
+      <Weather weatherData = {weatherData} />
     </div>
   );
+ } else {
+   const apiKey = "7f2bcb3fa67c76b6d051afd4ec0b0d33";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+   axios.get(apiUrl).then(weatherResponse);
+   return "Loading...";
+ }
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,29 @@
-import React, {useState} from "react";
-import axios from "axios";
+import React from "react";
 import FormatDate from "./FormatDate";
 import './Weather.css';
 
 export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ready : false});
-
-  function weatherResponse (response) {
-    console.log(response);
-    setWeatherData({
-      ready: true,
-      city: response.data.name,
-      date: new Date(response.data.dt * 1000),
-      iconUrl: "",
-      description: response.data.weather[0].description,
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity
-    });
-  }
-
-
-  
-  if (weatherData.ready) {
     return (
       <div className="Weather">
         <div className="header">
           <p>Good Morning</p>
-          <h1>{weatherData.city}</h1>
-          <p><FormatDate date={weatherData.date} /></p>
+          <h1>{props.weatherData.city}</h1>
+          <p><FormatDate date={props.weatherData.date} /></p>
         </div>
         <div className="row">
           <div className="col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-            <h3 className="icon-title text-capitalize">{weatherData.description}</h3>
+            <img src={props.weatherData.iconUrl} alt={props.weatherData.description} />
+            <h3 className="icon-title text-capitalize">{props.weatherData.description}</h3>
           </div>
           <div className="col-6">
-            <span className="temp"><strong>{Math.round(weatherData.temperature)}</strong></span><span className="unit"> ℃ | ℉</span>
+            <span className="temp"><strong>{Math.round(props.weatherData.temperature)}</strong></span><span className="unit"> ℃ | ℉</span>
             <ul>
               <li>Precipitation: 5%</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {Math.round(weatherData.wind)} mph</li>
+              <li>Humidity: {props.weatherData.humidity}%</li>
+              <li>Wind: {Math.round(props.weatherData.wind)} mph</li>
             </ul>
           </div>
         </div>
       </div>
   );
-  } else {
-    const apiKey = "7f2bcb3fa67c76b6d051afd4ec0b0d33";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(weatherResponse);
-    return(
-      "Loading..."
-    )
-  }
 }
